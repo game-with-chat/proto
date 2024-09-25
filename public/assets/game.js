136956449ec9d2ac5e63c9154816e99c964582cc
Tumble <tumblegamer@gmail.com>
2024-09-25 18:05:11 +0100

    added usernames

--- public/assets/game.js
+++ public/assets/game.js
@@ -1,4 +1,4 @@
-const { Application, Graphics } = PIXI;
+const { Application, Graphics,Container,Text } = PIXI;
 
 const app = new PIXI.Application();
 const socket = io();
@@ -8,21 +8,33 @@ let me;
 
 
 
-function createPlayer({id,pos=[0,0]}) {
-	console.log("new player")
-	const graphics = new Graphics()
+function createPlayer(player) {
+	console.debug("new player",player)
 
-	function move([x,y]) {
-		graphics.x = x - 10;
-		graphics.y = y - 10;
+	function move([x=app.canvas.width/2,y=app.canvas.height/2]=[]) {
+		container.x = x;
+		container.y = y;
 	}
-	graphics.rect(0, 0, 20, 20)
+
+	const container = new Container();
+	const graphics = new Graphics()
+	const username = new Text({ text: player.username,style: {
+		fontFamily: 'Arial',
+		fontSize: 15,
+		align: 'center',
+	} });
+
+	graphics.rect(-20,-45, 40, 40)
 	graphics.fill(0xffc107)
+	username.x =0;
+	username.y =0;
+	username.anchor.set(0.5,0);
+	container.addChild(graphics)
+	container.addChild(username)
+    app.stage.addChild(container);
 
-    app.stage.addChild(graphics);
-	move(pos)
-
-	return {id,move,leave,graphics}
+	move(player.pos)
+	return {id:player.id,move,container}
 }
 
 function click(e) {
@@ -36,40 +48,50 @@ async function start() {
 
     app.stage.eventMode = 'static';
     app.stage.hitArea = app.screen;
+
     app.stage.addEventListener('click', click);
 }
-
-function move(p) {
-	console.log("[socket:move]",p)
-	players[p.id]?.move(p.pos)
-}
-
-function room(room) {
-	console.log("[socket:room]",room)
-	room.forEach(p=> {
-		players[p.id] = createPlayer(p)
-	});	
-}
-
-function join(p) {
-	console.log("[socket:join]",p)
-	players[p.id] = createPlayer(p)
-	
-}
-function leave(id) {
-	console.log("[socket:leave]",id)
-	app.stage.removeChild(players[id].graphics);
-	delete players[id]
-	
-}
-
-start();
-
 socket.on("connect",()=>{
+	console.debug("[socket:connect]",socket.id)
 	me = socket.id
 })
 
-socket.on("move",move)
-socket.on("join",join)
-socket.on("leave",leave)
-socket.on("room",room)
+socket.on("move",p => {
+	console.debug("[socket:move]",p)
+	players[p.id]?.move(p.pos)
+})
+socket.on("join",p=> {
+	console.debug("[socket:join]",p)
+	players[p.id] = createPlayer(p)
+	
+})
+socket.on("leave",id=> {
+	console.debug("[socket:leave]",id)
+	app.stage.removeChild(players[id].container);
+	delete players[id]
+	
+})
+socket.on("room",room=>{
+	console.debug("[socket:room]",room)
+	room.forEach(p=> {
+		players[p.id] = createPlayer(p)
+	});	
+})
+socket.on("auth",()=> {
+	console.debug("[socket:auth]")
+	let username = localStorage.getItem("username");
+	if(!username) {
+		username = prompt("Enter a username")
+		localStorage.setItem("username",username)
+	} 
+	socket.emit("auth",username)
+})
+
+socket.on("welcome",()=>{
+	console.debug("[socket:welcome]")
+	socket.emit("join","face");
+})
+
+
+
+start();
--- src/index.js
+++ src/index.js
@@ -8,35 +8,53 @@ const server = createServer(app);
 const io = new Server(server);
 const players = {}
 
-
 app.use(express.static('public'))
 
 
 io.on("connection",socket=>{
-  console.log("[socket:connection]",socket.id)
-  
+  console.debug("[socket:connection]",socket.id)
+  const player = {id:socket.id};
+
+  function checkAuth() {
+    if(!("username" in player))
+      socket.emit("auth");
+    return "username" in player
+  }
+
+  socket.on("auth",auth=>{
+    console.debug("[socket:auth]",auth)
+    player.username = auth;
+    socket.emit("welcome",auth)
+  })
 
   socket.on("disconnect",()=>{
-    console.log("[socket:disconnect]",socket.id)
-    delete players[socket.id]
-    io.to("room").emit("leave",socket.id);
+    console.debug("[socket:disconnect]",socket.id)
+    if(!("room" in player)) return
+    delete players[player.room][player.id]
+    io.to(player.room).emit("leave",player.id);
   })
 
   socket.on("move",pos=>{
-    console.log("[socket:move]",pos);
+    console.debug(players)
+    console.debug("[socket:move]",pos);
+    if(!checkAuth()) return;
+    if(!("room" in player)) return
     player.pos = pos;
-    io.to("room").emit("move",player);
+    io.to(player.room).emit("move",player);
   })
 
-  const player = players[socket.id] = {
-    id:socket.id,
-    pos:[0,0]
-  }
-  io.to("room").emit("join",player)
-  socket.join("room")
-  socket.emit("room",Object.values(players))
+  socket.on("join",room=>{
+    console.debug("[socket:join]",room);
+    if(!checkAuth()) return;
+    players[room] ||= {};
+    players[room][socket.id] = player;
+    io.to(room).emit("join",player)
+    socket.join(room)
+    player.room = room;
+    socket.emit("room",Object.values(players[room]))
+    })
 })
 
 server.listen(port, () => {
-  console.log(`Server running http://localhost:${port}`)
+  console.debug(`Server running http://localhost:${port}`)
 })
